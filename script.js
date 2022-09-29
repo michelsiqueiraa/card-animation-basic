bf003bf9651ccc11e0da01023a079894f572e2b0
michelsiqueiraa <michelcarlos20@gmail.com>
2022-09-28 21:59:44 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const console = document.getElementById("console");
+const controle = document.getElementById("controle");
+
+const arrowRight = document.getElementById("right");
+const arrowLeft = document.getElementById("left");
+
+arrowLeft.addEventListener("click", () => {
+  console.style.left = "-650px";
+  controle.style.left = "75px";
+});
+arrowRight.addEventListener("click", () => {
+  console.style.left = "-50px";
+  controle.style.left = "650px";
+});
